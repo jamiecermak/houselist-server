242afd5215504a8e6cbf48449c2fa4d4f79551a2
Jamie Cermak <jamie.cermak.au@gmail.com>
2022-01-03 19:15:32 +1100

    style: refactor knexfile

--- knexfile.js
+++ knexfile.js
@@ -2,32 +2,21 @@ require('dotenv').config({
     path: `.env.${process.env.NODE_ENV}`,
 })
 
-module.exports = {
-    development: {
-        client: 'postgresql',
-        connection: process.env.HL_DB_CONNECTION_STRING,
-        pool: {
-            min: 2,
-            max: 10,
-        },
-        migrations: {
-            stub: './db/migrations.stub.js',
-            directory: './db/migrations',
-            tableName: 'knex_migrations',
-        },
+const BASE_CONFIG = {
+    client: 'postgresql',
+    connection: process.env.HL_DB_CONNECTION_STRING,
+    pool: {
+        min: 2,
+        max: 10,
     },
+    migrations: {
+        stub: './db/migrations.stub.js',
+        directory: './db/migrations',
+        tableName: 'knex_migrations',
+    },
+}
 
-    production: {
-        client: 'postgresql',
-        connection: process.env.HL_DB_CONNECTION_STRING,
-        pool: {
-            min: 2,
-            max: 10,
-        },
-        migrations: {
-            stub: './db/migrations.stub.js',
-            directory: './db/migrations',
-            tableName: 'knex_migrations',
-        },
-    },
+module.exports = {
+    development: BASE_CONFIG,
+    production: BASE_CONFIG,
 }
