563fd9b103700ac251e21cb36d8a68584714bcad
Jamie Cermak <jamie.cermak.au@gmail.com>
2022-01-05 21:28:40 +1100

    feat: IsAuthorised middleware

---
+++ middleware/IsAuthorised.js
@@ -0,0 +1,28 @@
+const { AuthorisationLib } = require('../lib/Authorisation')
+const { ServerAuthError } = require('../util/ServerErrors')
+
+async function IsAuthorised(req, res, next) {
+    const authHeader = req.get('Authorization')
+
+    if (!authHeader) {
+        next(new ServerAuthError('No Authoization Header'))
+    }
+
+    const authorisation = new AuthorisationLib()
+
+    try {
+        const userId = await authorisation.authoriseJWT(authHeader)
+
+        req.user = {
+            id: userId,
+        }
+    } catch (ex) {
+        next(ex)
+    }
+
+    next()
+}
+
+module.exports = {
+    IsAuthorised,
+}
---
+++ test/middleware/IsAuthorised.test.js
@@ -0,0 +1,54 @@
+const sinon = require('sinon')
+const { AuthorisationLib } = require('../../lib/Authorisation')
+const { IsAuthorised } = require('../../middleware/IsAuthorised')
+const { ServerAuthError, ServerError } = require('../../util/ServerErrors')
+
+describe('IsAuthorised Middleware', () => {
+    let req
+    let next
+    let authorisedJWT = sinon.stub(AuthorisationLib.prototype, 'authoriseJWT')
+
+    beforeEach(() => {
+        req = {
+            get: jest.fn(),
+        }
+
+        next = jest.fn()
+        authorisedJWT.reset()
+    })
+
+    it('will call next with ServerAuthError if no Authorization header provided', () => {
+        expect.assertions(2)
+
+        return IsAuthorised(req, {}, next).then(() => {
+            expect(req.get).toBeCalledWith('Authorization')
+            expect(next.mock.calls[0][0]).toBeInstanceOf(ServerAuthError)
+        })
+    })
+
+    it('will call next with ServerAuthError if an invalid token is provided', () => {
+        expect.assertions(2)
+
+        req.get.mockReturnValue('auth-token')
+        authorisedJWT.rejects(new ServerAuthError())
+
+        return IsAuthorised(req, {}, next).then(() => {
+            expect(authorisedJWT.calledWith('auth-token')).toBeTruthy()
+            expect(next.mock.calls[0][0]).toBeInstanceOf(ServerAuthError)
+        })
+    })
+
+    it('will call next with no error if a valid token is provided', () => {
+        const userId = 10
+
+        expect.assertions(2)
+
+        req.get.mockReturnValue('auth-token')
+        authorisedJWT.resolves(userId)
+
+        return IsAuthorised(req, {}, next).then(() => {
+            expect(req.user.id).toEqual(userId)
+            expect(next).toBeCalledWith()
+        })
+    })
+})
