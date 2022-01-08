406a2a6556a87025939a06c43a44374482c402da
Jamie Cermak <jamie.cermak.au@gmail.com>
2022-01-08 15:44:09 +1100

    feat: /user/change_password

--- e2e/routes/UserRoutes.test.js
+++ e2e/routes/UserRoutes.test.js
@@ -45,3 +45,91 @@ describe('GET /user', () => {
             })
     })
 })
+
+describe('POST /user/change_password', () => {
+    beforeAll(async () => {
+        await database.migrate.latest()
+        await database.seed.run()
+    })
+
+    afterAll(async () => {
+        await database.migrate.rollback()
+    })
+
+    it('call authorisation middleware', () => {
+        expect.assertions(1)
+
+        return request(app)
+            .post('/user/change_password')
+            .then(() => {
+                expect(IsAuthorised).toBeCalled()
+            })
+    })
+
+    it('returns 200 when given a valid same old password and new password', () => {
+        expect.assertions(3)
+
+        return request(app)
+            .post('/user/change_password')
+            .send({
+                new_password: 'new-password',
+                old_password: 'password',
+            })
+            .then((res) => {
+                expect(res.body).toEqual({
+                    success: true,
+                    data: null,
+                    message: null,
+                })
+                expect(res.statusCode).toEqual(200)
+
+                return request(app)
+                    .post('/auth/login')
+                    .send({
+                        username: 'johnsmith@example.com',
+                        password: 'new-password',
+                    })
+                    .then((res) => {
+                        expect(res.statusCode).toEqual(200)
+                    })
+            })
+    })
+
+    it('fails if old password is wrong', () => {
+        expect.assertions(2)
+
+        return request(app)
+            .post('/user/change_password')
+            .send({
+                new_password: 'new-password',
+                old_password: 'wrong-password',
+            })
+            .then((res) => {
+                expect(res.body).toEqual({
+                    success: false,
+                    message:
+                        'There was an issue processing your request. (Your password is incorrect)',
+                    data: null,
+                })
+                expect(res.statusCode).not.toEqual(200)
+            })
+    })
+
+    it('fails if wrong payload', () => {
+        expect.assertions(2)
+
+        return request(app)
+            .post('/user/change_password')
+            .send({
+                new_password: 'new-password',
+            })
+            .then((res) => {
+                expect(res.body).toEqual({
+                    success: false,
+                    message: 'There was an issue processing your request.',
+                    data: null,
+                })
+                expect(res.statusCode).not.toEqual(200)
+            })
+    })
+})
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,6 +2,10 @@ const { SuccessResponse } = require('../util/APIResponses')
 const { ErrorHandler } = require('../middleware/ErrorHandler')
 const { IsAuthorised } = require('../middleware/IsAuthorised')
 const { UsersLib } = require('../lib/Users')
+const { PayloadValidator } = require('../middleware/PayloadValidator')
+const yup = require('yup')
+const { AuthenticationLib } = require('../lib/Authentication')
+const { ServerValidationError } = require('../util/ServerErrors')
 const router = require('express').Router()
 
 router.get(
@@ -13,6 +17,51 @@ router.get(
         const user = await users.getActiveUserById(req.user.id)
 
         const response = new SuccessResponse(user)
+        response.send(res)
+    }),
+)
+
+router.post(
+    '/change_password',
+    IsAuthorised,
+    PayloadValidator(
+        yup
+            .object()
+            .shape({
+                old_password: yup
+                    .string()
+                    .max(255, 'Old Password must be 255 characters or less')
+                    .required('Old Password is required'),
+                new_password: yup
+                    .string()
+                    .max(255, 'New Password must be 255 characters or less')
+                    .required('New Password is required'),
+            })
+            .noUnknown(true, 'Unknown options')
+            .strict(true),
+    ),
+    ErrorHandler(async (req, res) => {
+        const { new_password, old_password } = req.payload.body
+
+        const users = new UsersLib()
+        const user = await users.getActiveUserById(req.user.id)
+        const authentication = new AuthenticationLib()
+
+        try {
+            await authentication.authenticateUser(
+                user.email_address,
+                old_password,
+            )
+        } catch (ex) {
+            throw new ServerValidationError(
+                'Invalid Old Password',
+                'Your password is incorrect',
+            )
+        }
+
+        await users.setPassword(req.user.id, new_password)
+
+        const response = new SuccessResponse()
         response.send(res)
     }),
 )
